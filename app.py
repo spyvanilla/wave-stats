17f0279d366014877dcb4770ecc31dcd46638b34
Levi Barros <levisbarros1234@gmail.com>
2022-10-15 11:06:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def serve():
     """
     Only works if there's a build folder created in the client side,
     if a new url is added on the client, it needs to be in the app routes
-    like the '/' and '/profile' routes 
+    like the '/' and '/profile' routes
     """
 
     return send_from_directory(app.static_folder, 'index.html')
